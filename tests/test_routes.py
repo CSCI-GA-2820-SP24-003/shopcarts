d94ec74cc6c33e3048e9b952830827ad5ee77d94
Tanya8527 <tanyaojha97@gmail.com>
2024-03-03 18:35:07 +0000

    Added delete shopcart endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -129,3 +129,15 @@ class TestShopCartService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 10)
+
+    def test_delete_shopcart(self):
+        """It should Delete a Shopcart"""
+        test_shopcart = self._create_shopcarts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{test_shopcart.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+
+        # TODO: uncomment when get_shopcarts is implemented
+        # response = self.client.get(f"{BASE_URL}/{test_shopcart.id}")
+        # self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
